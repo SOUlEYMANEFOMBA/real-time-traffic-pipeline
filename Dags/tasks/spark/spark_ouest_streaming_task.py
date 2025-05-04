c15ad91bbdac25f7c732c6ba71be9fab294c2f41
SOUlEYMANEFOMBA <souleymanefmb@gmail.com>
2025-05-04 21:23:35 +0000

    feat: create spark streaming task

---
+++ Dags/tasks/spark/spark_ouest_streaming_task.py
@@ -0,0 +1,28 @@
+import logging
+
+
+class SparkOuestStreamingTask():
+    
+    """_summary_
+    """
+    def connect_to_kafka_topic_users_ouest_created(self, spark_connect):
+        """
+        Crée un DataFrame Spark en consommant les données depuis Kafka.
+
+        :param spark_connect: La session Spark active
+        :return: Un DataFrame Spark, ou None en cas d'erreur
+        """
+        logging.info("Beginning of Kafka DataFrame creation...")
+        spark_df = None
+        try:
+            spark_df = spark_connect.readStream \
+                .format('kafka') \
+                .option('kafka.bootstrap.servers', 'broker:29092') \
+                .option('subscribe', 'users_ouest_created') \
+                .option('startingOffsets', 'earliest') \
+                .load()
+            logging.info("Kafka DataFrame created successfully!")
+        except Exception as e:
+            logging.warning(f"Kafka DataFrame could not be created due to: {e}")
+
+        return spark_df
